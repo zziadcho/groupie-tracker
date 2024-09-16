93c148414eef109f9012695eef129b3d315538d6
zziadcho <ziad.choukrii@gmail.com>
2024-09-16 01:10:04 +0100

    new: - directory "utils" - temporary coe to test unmarshalling data to a map

--- common/functions/MainHandler.go
+++ common/functions/MainHandler.go
@@ -1,33 +1,14 @@
 package functions
 
 import (
+	//"01/groupie-tracker/common/utils"
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"path/filepath"
 )
-
-type Data struct {
-	Artists   string
-	Locations string
-	Dates     string
-	Relation  string
-}
-
-var UserData Data
-
-type Artists struct {
-	Id           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creation_date"`
-	FirstAlbum   string   `json:"first_album"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concert_dates"`
-	Relations    string   `json:"relations"`
-}
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -40,27 +21,57 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer response.Body.Close()
 
 	responseData, err := io.ReadAll(response.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	var responseObject []Artists
+	/////////////////////////////////////////////////////////////////////
+
+	var responseObject map[string]interface{}
 	if err := json.Unmarshal(responseData, &responseObject); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	// for key, value := range responseObject {
+	
+	// }
+
 	tmpl := template.Must(template.ParseFiles(filepath.Join("./common/static/index.html")))
 	if err := tmpl.Execute(w, responseObject); err != nil {
-		http.Error(w, "error executing template", http.StatusInternalServerError)
+		http.Error(w, "error executing the template", http.StatusInternalServerError)
 	}
+
+	// response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// defer response.Body.Close()
+
+	// responseData, err := io.ReadAll(response.Body)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// var responseObject []Artists
+	// if err := json.Unmarshal(responseData, &responseObject); err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// tmpl := template.Must(template.ParseFiles(filepath.Join("./common/static/index.html")))
+	// if err := tmpl.Execute(w, responseObject); err != nil {
+	// 	http.Error(w, "error executing template", http.StatusInternalServerError)
+	// }
 }

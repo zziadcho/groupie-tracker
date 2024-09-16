adaace37140595cc7c77780189b6b0fbccfd546c
zziadcho <ziad.choukrii@gmail.com>
2024-09-16 01:07:12 +0100

    new: usage of path/filepath package to serve the needed files

--- main.go
+++ main.go
@@ -4,18 +4,21 @@ import (
 	"01/groupie-tracker/common/functions"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 const PORT = ":8080"
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", functions.MainHandler)
+	staticDir := filepath.Join("common", "static") 
+    fs := http.FileServer(http.Dir(staticDir))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Println("Server is running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+
 }
 
 	
